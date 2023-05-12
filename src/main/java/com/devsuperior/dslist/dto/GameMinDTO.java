6a11a6de779a6e71ca9a9db7c7d0cbbe9397ecdb
Nelio Alves <acenelio@gmail.com>
2023-05-12 13:19:10 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -22,7 +22,7 @@ public class GameMinDTO {
 	public GameMinDTO(GameMinProjection projection) {
 		id = projection.getId();
 		title = projection.getTitle();
-		year = projection.getYear();
+		year = projection.getGameYear();
 		imgUrl = projection.getImgUrl();
 		shortDescription = projection.getShortDescription();
 	}
--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
