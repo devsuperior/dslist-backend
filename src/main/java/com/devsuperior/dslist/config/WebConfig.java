1385cccb0fbbc3969015497878028f001a4087cf
Nelio Alves <acenelio@gmail.com>
2023-05-12 13:20:10 -0300

    CORS config

---
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+
+}
